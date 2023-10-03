d4329fc6817a0a8a8873921a0276c99eff8001ac
chungesudeshna <141159202+chungesudeshna@users.noreply.github.com>
2023-10-03 19:34:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
 
-provider "aws" {
-  # Configuration options
-}
 provider "aws" {
 access_key ="var.accesskey"
 secret_key ="var.secretkey"
